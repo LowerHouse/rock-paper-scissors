2a009974e2cf078275bb9efe416f787fbe638d18
LowerHouse <mr.alexzilla@gmail.com>
2023-10-31 22:17:30 +0200

    add game logic

--- app.js
+++ app.js
@@ -4,4 +4,25 @@ function getComputerChoice(){
     return options[Math.floor(((Math.random() * 3)))]
 }
 
-console.log(getComputerChoice())
+let playerSelection = 'scissors'
+let computerSelection = getComputerChoice()
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    player = options.findIndex((element) => element === playerSelection)
+    computer = options.findIndex((element) => element === computerSelection)
+    
+    if(player == computer +1 | player == computer - 2){
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+
+    else if(player == computer){
+        return `Draw! you both had ${playerSelection}`
+    }
+
+    else{
+        return `You loose!  ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection))
